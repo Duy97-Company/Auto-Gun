50b53a0666a364d377a7ec9238dea077c067422e
duyphaphach <duyphamle97@gmail.com>
2020-01-11 05:30:53 +0700

    update test for improve coverage

--- unit.test.js
+++ unit.test.js
@@ -1,9 +1,13 @@
+const normalizePath = require('./normalize-path');
+
 describe('Should render Login Page', () => {
   beforeEach(async () => {
 
   }, 30000);
 
   it('run the command', async () => {
-    expect(true).toBe(true);
+    const path = 'C:/abc';
+    const normalizedPath = normalizePath(path);
+    expect(normalizedPath).toBe('C:/abc\\');
   }, 30000);
 });
